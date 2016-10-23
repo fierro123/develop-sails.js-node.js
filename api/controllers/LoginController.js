f289c137163d56c48827e3c37ebdc5a1ef87ee64
FelixMH <felixmtzh.fm@gmail.com>
2016-10-23 00:57:04 -0500

    Controlador Login y configuraciones para renderizar vista

---
+++ api/controllers/LoginController.js
@@ -0,0 +1,23 @@
+/**
+ * LoginController
+ *
+ * @description :: Server-side logic for managing logins
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	validar: function(req, res) {
+
+		if(req.method == 'POST'){
+			var user = req.param('user');
+			var pass = req.param('pass');
+
+			return res.send({ saludo : 'Hola Mundo', datos : user + ',' + pass })
+		}else {
+			return res.redirect('/login');
+		}
+
+    return res.json({
+    });
+  },
+};
